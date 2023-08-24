42d4631edcb7a96b52aaad831e7aa2b0fd75bb76
Akshit Aryan <aryanakshit152@gmail.com>
2023-08-25 00:51:49 +0530

    selection color change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ import Quote from "./quote";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full min-h-screen space-y-10 text-white bg-black">
+    <main className="flex flex-col w-full min-h-screen space-y-10 text-white bg-black selection:text-black selection:bg-red-700">
       <HomePage />
       <AboutMe />
       <Quote />
