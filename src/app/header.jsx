8655838ca4cbfa3f2b6a8b90687fcaaf42baf8bb
Akshit Aryan <aryanakshit152@gmail.com>
2023-08-27 16:22:00 +0530

    header fix

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full flex text-white justify-between bg-[#0000004b]">
-      <span>Aa</span>
-      <span>about, services, contact</span>
+    <div className="w-full flex text-white justify-between px-10 py-3 bg-gradient-to-b from-[#75000067] to-[#000000]">
+      <span className="text-[1.7vw] font-extrabold font-poppins group cursor-pointer">
+        <span className="transition-all group-hover:text-red-700">akshit</span>
+        <span className="ml-1 text-red-700 transition-all group-hover:text-white">
+          aryan
+        </span>
+      </span>
+      <span className="text-[1.5vw]">about, services, contact</span>
     </div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import AboutMe from "./about";
 import Bored from "./bored";
 import Contact from "./contact";
+import Header from "./header";
 import HomePage from "./home";
 import NasaPotd from "./photoNasa";
 import HomeProjects from "./projects";
@@ -9,6 +10,7 @@ import Quote from "./quote";
 export default function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen space-y-10 text-white bg-black selection:text-black selection:bg-red-700">
+      <Header />
       <HomePage />
       <AboutMe />
       <Quote />
