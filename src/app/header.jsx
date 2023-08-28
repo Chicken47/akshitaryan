fc04df0b9827b0f3edd12be8da5ce91a4f38b91a
Akshit Aryan <aryanakshit152@gmail.com>
2023-08-28 22:10:06 +0530

    added scrollTo

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -1,9 +1,13 @@
+"use client";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full flex text-white justify-between px-10 py-3 bg-gradient-to-b from-[#75000067] to-[#000000]">
-      <span className="text-[1.7vw] font-extrabold font-poppins group cursor-pointer">
+    <div className="w-full flex fixed text-white justify-between px-10 py-3 bg-gradient-to-b from-[#75000067] to-transparent z-10">
+      <span
+        onClick={() => window.scrollTo(top)}
+        className="text-[1.7vw] font-extrabold font-poppins group cursor-pointer"
+      >
         <span className="transition-all group-hover:text-red-700">akshit</span>
         <span className="ml-1 text-red-700 transition-all group-hover:text-white">
           aryan
